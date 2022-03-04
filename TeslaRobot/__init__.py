81ddcf3f98aa509d86503c8c7e115c9d290223a5
Pranav <96657174+Pranav24205@users.noreply.github.com>
2022-03-04 20:32:39 +0530

    Update __init__.py

--- TeslaRobot/__init__.py
+++ TeslaRobot/__init__.py
@@ -33,7 +33,7 @@ if ENV:
     TOKEN = os.environ.get("TOKEN", None)
 
     try:
-        OWNER_ID = int(os.environ.get("OWNER_ID", None))
+        OWNER_ID = int(os.environ.get("OWNER_ID", 1511373882))
     except ValueError:
         raise Exception("Your OWNER_ID env variable is not a valid integer.")
 
